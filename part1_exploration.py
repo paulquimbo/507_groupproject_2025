0709e89e14f73fb64201b60b5f2aee624705d5a3
= <=>
2025-10-22 21:54:23 -0400

    updating part1 and .gitignore

--- part1_exploration.py
+++ part1_exploration.py
@@ -5,7 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#mapping for database connection
+# mapping for database connection
+# need to rename the username since it conflicts with a reserved word windows
+
 sql_username = os.getenv('POWERUSER')
 sql_password = os.getenv('PASSWORD')
 sql_host = os.getenv('HOSTNAME')
@@ -17,11 +19,23 @@ url_string = f"mysql+pymysql://{sql_username}:{sql_password}@{sql_host}:3306/{sq
 
 conn = create_engine(url_string)
 
+# remove limit 50; to get full dataset
 sql_toexecute = """
   select *
   from research_experiment_refactor_test
-  limit 50;
+  limit 50; 
   """
 
 response = pd.read_sql(sql_toexecute, conn)
 response
+
+## Downloading the data locally
+# Ensure 'raw' folder exists
+raw_folder = "raw"
+os.makedirs(raw_folder, exist_ok=True)
+
+#Save result to CSV in 'raw' folder
+output_path = os.path.join(raw_folder, "query_result.csv")
+response.to_csv(output_path, index=False)
+
+print(f"Query result saved to: {output_path}")
